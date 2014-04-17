9fc6241186c08b13ab9d3332fdb289876374ef4c
Scott Garrison <scott@allpowerlabs.org>
2014-04-17 13:17:55 -0700

    breaking up project into components

--- charon.py
+++ charon.py
@@ -3,86 +3,12 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.exc import NoResultFound #, MultipleResultsFound
 import json, datetime, sys
 
-from flask.ext.login import LoginManager, login_user, logout_user, login_required
-import hashlib
-from os import urandom, path
-from base64 import b64encode, b64decode
-from itertools import izip
-
 from flask.ext.uploads import UploadSet, IMAGES, AUDIO, configure_uploads  #http://pythonhosted.org/Flask-Uploads/
-
-# From https://github.com/mitsuhiko/python-pbkdf2
-from pbkdf2 import pbkdf2_bin
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/charon.db'  #change to postgres database
 app.config['SECRET_KEY'] = '123charon456'
 db = SQLAlchemy(app)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-#
-# Password Hashing and Login
-#
-
-# Parameters to PBKDF2. Only affect new passwords.
-SALT_LENGTH = 12
-KEY_LENGTH = 24
-HASH_FUNCTION = 'sha256'  # Must be in hashlib.
-# Linear to the hashing time. Adjust to be high but take a reasonable
-# amount of time on your server. Measure with:
-# python -m timeit -s 'import passwords as p' 'p.make_hash("something")'
-COST_FACTOR = 10000
-
-
-def make_hash(password):
-    """Generate a random salt and return a new hash for the password."""
-    if isinstance(password, unicode):
-        password = password.encode('utf-8')
-    salt = b64encode(urandom(SALT_LENGTH))
-    return 'PBKDF2${}${}${}${}'.format(
-        HASH_FUNCTION,
-        COST_FACTOR,
-        salt,
-        b64encode(pbkdf2_bin(password, salt, COST_FACTOR, KEY_LENGTH,
-                             getattr(hashlib, HASH_FUNCTION))))
-
-def check_hash(password, hash_):
-    """Check a password against an existing hash."""
-    if isinstance(password, unicode):
-        password = password.encode('utf-8')
-    algorithm, hash_function, cost_factor, salt, hash_a = hash_.split('$')
-    assert algorithm == 'PBKDF2'
-    hash_a = b64decode(hash_a)
-    hash_b = pbkdf2_bin(password, salt, int(cost_factor), len(hash_a),
-                        getattr(hashlib, hash_function))
-    assert len(hash_a) == len(hash_b)  # we requested this from pbkdf2_bin()
-    # Same as "return hash_a == hash_b" but takes a constant time.
-    # See http://carlos.bueno.org/2011/10/timing.html
-    diff = 0
-    for char_a, char_b in izip(hash_a, hash_b):
-        diff |= ord(char_a) ^ ord(char_b)
-    return diff == 0
-
-def try_login_user(email, password):
-    if not password:
-        return None
-    try:
-        user = User.query.filter_by(email=email).one()
-    except NoResultFound:
-        return None
-    if not check_hash(password, user.password):
-        return None
-    login_user(user)
-    return user
-
-
-@login_manager.user_loader
-def load_user(userid):
-    return User.query.get(int(userid))
-
-login_manager.login_view = "login"
 
 #
 # Models
@@ -125,58 +51,40 @@ class Parent(db.Model):
     street = db.Column(db.String(80))
     city = db.Column(db.String(80))
     zip = db.Column(db.Integer) #better fix width type?
-    email = db.Column(db.String(120))
-    password = db.Column(db.String(64))
-    oath = db.Column(db.String(64)) # to be determined
+    user = db.Column(db.Integer) #relationship??
     phone = db.Column(db.String(14))
     children = db.relationship('Student', back_populates="parents") # backref=db.backref('parents')
 
 
-    def __init__(self, f_name, l_name, street, city, zip, email, password, oath, phone, children):
+    def __init__(self, f_name, l_name, street, city, zip, user, phone, children):
         self.f_name = f_name
         self.l_name = l_name
         self.street = street
         self.city = city
         self.zip = zip
-        self.email = email
-        self.password = password
-        self.oath = oath
+        self.user = user
         self.phone = phone
         self.children = children
         
     def __repr__(self):
         return '<Parent %r %r>' % self.f_name, self.l_name
 
-
-#
-# Views
-#
-
-@app.route('/')
-@app.route('/index')
-def index():
-    """Return the main view."""
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = try_login_user(email, password)
-        if user is None:
-            error = u'Login failed'
-            flash(error)
-        else:
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    flash(u'You were logged out')
-    return redirect(url_for('index'))
-
+class Student(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(80))
+    l_name = db.Column(db.String(80))
+    siblings = db.relationship('Student', 
+    parents = db.relationship('Parent', backref='children')
+    probability = db.Column(db.Float)
+    gender = db.Column(db.Integer) #0-Female, 1-Male; Use Enum in Postgres??
+    dob = db.Column(db.Date)
+    finaid = 
+    state
+    classroom = 
+    friends = relationship("Student",
+                    secondary=friends_table,
+                    backref="friends")
+    good_influences
+    stretches
+    intellectual_peers
---
+++ views.py
@@ -0,0 +1,62 @@
+#
+# Views
+#
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    """Return the main view."""
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = try_login_user(email, password)
+        if user is None:
+            error = u'Login failed'
+            flash(error)
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash(u'You were logged out')
+    return redirect(url_for('index'))
+
+
+@app.route('/parent_portal/',  methods=['GET', 'POST'])
+def portal():
+    pass
+
+
+#
+# APIs
+#   
+@app.route('api/parent/<parent>', methods=['GET', 'POST', 'DELETE', 'UPDATE'])
+def parent(parent):
+    pass
+    
+    
+@app.route('api/student/<student>', methods=['GET', 'POST', 'DELETE', 'UPDATE'])
+def student(student):
+    pass
+    
+@app.route('api/location/<location>', methods=['GET', 'POST', 'DELETE', 'UPDATE'])
+def location(location):
+    pass
+    
+@app.route('api/location/<location>/class/<class>', methods=['GET', 'POST', 'DELETE', 'UPDATE'])
+def class(location, class):
+    pass
+    
+    
+@app.route(
